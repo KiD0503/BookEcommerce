fae1df2d1f61107aab7c22414c9ae926ee26f8d2
kid <hieunv21@gmail.com>
2022-03-17 18:36:49 +0700

    Code User Authentication (not yet)

---
+++ BookWebParent/BookBackEnd/src/main/java/com/book/admin/security/BookUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.book.admin.security;
+
+import com.book.admin.user.UserRepository;
+import com.book.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class BookUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.getUserByEmail(email);
+        if (user != null){
+            return new BookUserDetails(user);
+        }
+        throw new UsernameNotFoundException("Could not find user with email: " + email);
+    }
+}

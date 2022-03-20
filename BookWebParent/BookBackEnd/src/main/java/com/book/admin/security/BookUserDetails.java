688803661ee4780e49dc6564b7fc39fde03559ab
kid <hieunv21@gmail.com>
2022-03-20 10:31:42 +0700

    Code Update Account Details

--- BookWebParent/BookBackEnd/src/main/java/com/book/admin/security/BookUserDetails.java
+++ BookWebParent/BookBackEnd/src/main/java/com/book/admin/security/BookUserDetails.java
@@ -63,4 +63,12 @@ public class BookUserDetails implements UserDetails {
     public String getFullname(){
         return this.user.getFirstName() + " " + this.user.getLastName();
     }
+
+    public void setFirstName (String firstName){
+        this.user.setFirstName(firstName);
+    }
+
+    public void setLastName (String lastName){
+        this.user.setLastName(lastName);
+    }
 }

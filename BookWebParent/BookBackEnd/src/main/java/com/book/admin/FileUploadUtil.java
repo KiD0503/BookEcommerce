026ae0befdf9628549b5b548f6ef54af1f25f800
kid <hieunv21@gmail.com>
2022-03-21 11:42:36 +0700

    Code Category Entity and CategoryRepository

--- BookWebParent/BookBackEnd/src/main/java/com/book/admin/FileUploadUtil.java
+++ BookWebParent/BookBackEnd/src/main/java/com/book/admin/FileUploadUtil.java
@@ -2,6 +2,8 @@ package com.book.admin;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -10,6 +12,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class FileUploadUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
     public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
         Path uploadPath  = Paths.get(uploadDir);
         if (!Files.exists(uploadPath)){
@@ -30,12 +34,14 @@ public class FileUploadUtil {
                     try{
                         Files.delete(file);
                     }catch (IOException ioException){
-                        System.out.println("Could not delete file: " + file);
+                        LOGGER.error("Could not delete file: " + file);
+                        //System.out.println("Could not delete file: " + file);
                     }
                 }
             });
         }catch (IOException ioException){
-            System.out.println("Could not list directory: " + dirPath);
+            LOGGER.error("Could not list directory: " + dirPath);
+            //System.out.println("Could not list directory: " + dirPath);
         }
     }
 }

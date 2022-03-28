d321da0a2ad39580918815185e16070d489e0e84
kid <hieunv21@gmail.com>
2022-03-28 16:57:49 +0700

    Code Product entity

---
+++ BookWebParent/BookBackEnd/src/main/java/com/book/admin/user/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.book.admin.user.repository;
+
+import com.book.common.entity.Product;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface ProductRepository extends PagingAndSortingRepository<Product,Integer> {
+}

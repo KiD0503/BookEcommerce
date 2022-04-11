1bd97d7eb42feb3d25084974566b1574cf5f545d
kid <hieunv21@gmail.com>
2022-04-11 12:46:31 +0700

    Code search products function

--- BookWebParent/BookFrontEnd/src/main/java/com/book/service/ProductService.java
+++ BookWebParent/BookFrontEnd/src/main/java/com/book/service/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
     public static final int PRODUCTS_PER_PAGE = 10;
+    public static final int SEARCH_RESULTS_PER_PAGE = 10;
 
     @Autowired
     private ProductRepository productRepository;
@@ -31,5 +32,11 @@ public class ProductService {
         }
 
         return product;
+    }
+
+    public Page<Product> search(String keyword, int pageNum) {
+        Pageable pageable = PageRequest.of(pageNum - 1, SEARCH_RESULTS_PER_PAGE);
+        return productRepository.search(keyword, pageable);
+
     }
 }
